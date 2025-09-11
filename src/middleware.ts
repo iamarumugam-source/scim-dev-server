2d3daf362b148dc2aa3a2c4241a607aca2321cdd
Arumugam <nenjunane@gmail.com>
2025-09-11 22:14:23 +0530

    rewriting middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,24 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
+
+// export const config = {
+//   matcher: ['/((?!api/scim|_next/static|_next/image|favicon.ico).*)'],
+// }
+
+
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+
+  function middleware(req: NextRequestWithAuth) {
+
+    return NextResponse.next();
+  },
+  {
+    // You can add callbacks here for more granular control if needed.
+  }
+);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
+  matcher: ["/((?!api).*)"],
+};
