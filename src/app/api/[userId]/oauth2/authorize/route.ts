61351c48a430a1a973d7e55b492f54c2088a5425
Arumugam <nenjunane@gmail.com>
2025-09-30 10:32:49 +0530

    testing oauth

--- src/app/api/[userId]/oauth2/authorize/route.ts
+++ src/app/api/[userId]/oauth2/authorize/route.ts
@@ -27,5 +27,37 @@ export async function GET(request: NextRequest) {
     return NextResponse.redirect(new URL(redirect_url), { status: 302 });
   }
 
+  const OKTA_SIGNING_SECRET = process.env.OKTA_SIGNING_SECRET;
+
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Basic ${btoa(
+      OKTA_SIGNING_CLIENT + ":" + OKTA_SIGNING_SECRET
+    )}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  if (hasCode) {
+    const codeVal = searchParams.get("code") as string;
+    const body = new URLSearchParams({
+      grant_type: "authorization_code",
+      redirect_uri:
+        "https://okta-inbound-scim.vercel.app/api/00u1x48tm4aH3UPmQ1d8/oauth2/authorize",
+      code: codeVal,
+    });
+
+    const tokenCall = await fetch("https://arumugam.okta.com/oauth2/v1/token", {
+      method: "POST",
+      headers: headers,
+      body: body.toString(),
+    });
+
+    if (!tokenCall.ok) {
+      throw new Error(`HTTP error! Status: ${tokenCall.status}`);
+    }
+    const token = await tokenCall.json();
+    return NextResponse.json(token, { status: 200 });
+  }
+
   return NextResponse.json({}, { status: 200 });
 }
